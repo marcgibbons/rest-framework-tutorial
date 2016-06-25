d1aaff8b70564ad789c28b012e2967965bf6aac0
Marc Gibbons <marc_gibbons@rogers.com>
2016-06-25 19:52:02 -0400

    Test out Swagger renderer

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,8 +1,17 @@
 from snippets import views
 from django.conf.urls import patterns, url, include
+
+from coreapi_swagger.contrib.django.renderers import SwaggerRenderer
+from rest_framework.renderers import CoreJSONRenderer
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+# TODO: this is a quick hack for demo
+# would want to allow schema_renderers to be set from the init.
+class SwaggerRouter(DefaultRouter):
+    schema_renderers = [CoreJSONRenderer, SwaggerRenderer]
+
+
+router = SwaggerRouter(schema_title='Pastebin API')
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 

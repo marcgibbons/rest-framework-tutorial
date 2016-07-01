178bce9ca504112f908b2632211307d7b12c1081
Marc Gibbons <marc_gibbons@rogers.com>
2016-07-01 15:21:34 -0400

    Add schema generator and use Swagger UI + OpenAPI renderers

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,21 +1,25 @@
-from snippets import views
-from django.conf.urls import patterns, url, include
-
-from coreapi_swagger.contrib.django.renderers import SwaggerRenderer
-from rest_framework.renderers import CoreJSONRenderer
+from coreapi_swagger.contrib.django.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from django.conf.urls import url, include
+from rest_framework import response, schemas
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.routers import DefaultRouter
 
-# TODO: this is a quick hack for demo
-# would want to allow schema_renderers to be set from the init.
-class SwaggerRouter(DefaultRouter):
-    schema_renderers = [CoreJSONRenderer, SwaggerRenderer]
+from snippets import views
 
 
-router = SwaggerRouter(schema_title='Pastebin API')
+@api_view()
+@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer])
+def schema_view(request):
+    generator = schemas.SchemaGenerator(title='Pastebin API')
+    return response.Response(generator.get_schema(request=request))
+
+
+router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
-urlpatterns = patterns('',
+urlpatterns = [
+    url('^$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
+]
